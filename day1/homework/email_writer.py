760596695d1f04256473a6d12ad54f0f1efdf0b1
SRMartinJ <p6marjon@SR150423.sr.se>
2015-09-21 20:13:33 +0200

    Martins försök
    Ville gärna runda av och slippa decimaler i rubriken. Men det kanske vi
    kan ta senare. M

--- day1/homework/email_writer.py
+++ day1/homework/email_writer.py
@@ -6,12 +6,15 @@ Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 
 
 def emailify(name, domain):
-    """
-     Skriv din kod här!
-    """
+	print(name.lower().replace(" ", ".")+"@"+domain)
 
 emailify("Annie Lööf", "riksdagen.se")
 emailify("David Lång", "riksdagen.se")
 emailify("Emma Nohrén", "riksdagen.se")
 emailify("阿部慎太郎", "asahi.co.jp")
 emailify("Östen Rubin", "dn.se")
+
+
+name=("Martin Jonsson")
+
+print(name.lower().replace(" ", "."))
