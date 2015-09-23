c8b512960a55864f942e409ec3a44ccd1be6707a
SRMartinJ <p6marjon@SR150423.sr.se>
2015-09-23 09:03:27 +0200

    Nytt försök

--- day1/homework/email_writer.py
+++ day1/homework/email_writer.py
@@ -5,19 +5,13 @@
 Skriv en funktion som tar ett namn och en domän.
 Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 """
-import unicodedata as ud
 
 def emailify(name, domain):
-	print(name.lower().replace(" ,å,ä,ö", ".,a,a,o")+"@"+domain)
+	print(name.lower().replace(" ", ".").replace("å", "a").replace("ä", "a").replace("ö", "o").replace("é", "e").replace("Ö", "o")+"@"+domain)
 
 emailify("Annie Lööf", "riksdagen.se")
 emailify("David Lång", "riksdagen.se")
 emailify("Emma Nohrén", "riksdagen.se")
 emailify("阿部慎太郎", "asahi.co.jp")
 emailify("Östen Rubin", "dn.se")
-
-
-name=(u"Martin Jönsson")
-
-print(name.lower().replace(" ", "."))
 
